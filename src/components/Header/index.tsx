e59810855e304c1e426098d4ce12ce2c1b0bfe99
Brambs <gabriel128.br@gmail.com>
2024-03-28 09:38:51 -0300

    Routes and router
    DefaultLayout routes added

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { HeaderContainer } from './styles'
+import logo from '../../assets/Logo.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import History from '../pages/History'
+import { DefaultLayout } from '../layouts/DefaultLayout'
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
